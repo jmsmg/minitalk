a370bf548c2fb30cb1e1c2323b6ffc62d46eb8ea
jmsmg <jmsmg1@me.com>
2023-09-13 15:27:20 +0900

    server handler 구현해야함

--- client/client.c
+++ client/client.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:27:13 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/09/12 17:34:06 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:20:48 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,13 @@ int	main(int argc, char *argv[])
 	char	message;
 
 	if (argc != 3)
+	{
+		ft_putstr_fd("fail", 1);
 		return (1);
-
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid < 2 && 32768 < pid)
+		return (1);
 	message = argv[2];
 
 }
--- client/client.h
+++ client/client.h
@@ -1,5 +1,8 @@
+
 #ifndef CLIENT_H
 
+#define CLIENT_H
 #include <signal.h>
-// #include <sys/types.h>
 #include <unistd.h>
+
+#endif
